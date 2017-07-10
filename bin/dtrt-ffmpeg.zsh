25f8d7abaf14a9e77b55f055cdd111a6bfc46411
Marc Chantreux <mc@unistra.fr>
2017-07-10 11:19:42 +0200

    all default values now lives in dtrt-ffmpeg.zsh

--- bin/dtrt-ffmpeg.zsh
+++ bin/dtrt-ffmpeg.zsh
@@ -1,12 +1,21 @@
-setopt nounset warncreateglobal
+setopt nounset warncreateglobal extendedglob braceccl
 
+use_slurm=false use_cuvid=false
+output_template='${input:t:r}_${width}x${height}.${input:e}'
 
-use_slurm=false
-use_cuvid=false
+encoder=( ffmpeg )
+available_sizes=(
+    320.240
+    720.480
+    1280.720
+    1920.1080
+)
+
 workdir=.
 
-alias @='for it'
-alias @-='while {read it}'
+# outch ... doesn't work with old versions of zsh :(
+# alias @='for it'
+# alias @-='while {read it}'
 
+# TODO: XDG support ?
 for it ( ~/.dtrt-ffpmeg(N) ) . $it
-
