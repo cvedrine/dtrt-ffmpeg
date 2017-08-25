7778edfb0d2c8af5a72b156f82f8437ffcf45d25
Marc Chantreux <mc@unistra.fr>
2017-08-25 10:38:18 +0200

    now compatible with older zsh versions

--- bin/rest-pod.zsh
+++ bin/rest-pod.zsh
@@ -1,12 +1,15 @@
 . rest-base.zsh
 
 info () {
-    rest_client+="$WS/${1?id of the video}.json"
+    rest_client+=("$WS/${1?id of the video}.json")
     shift
     "$@"
 }
 
 encoding_in_progress () {
     rest_client+=( -XPATCH -d '{ "encoding_in_progress" : '$1'  }' )
-    "$@" }
+    shift
+    "$@"
+}
+
 
