1902bc8f5aa2828b48a7e856cb220fc6a020cda1
Marc Chantreux <mc@unistra.fr>
2017-09-04 16:14:02 +0200

    mOAR todo items

--- bin/dtrt-ffmpeg.zsh
+++ bin/dtrt-ffmpeg.zsh
@@ -3,6 +3,15 @@ setopt nounset warncreateglobal extendedglob braceccl
 use_slurm=false use_cuvid=false
 output_template='${input:t:r}_${width}x${height}.${input:e}'
 
+video_codec_name () {
+    ffprobe -v error -select_streams v:0 \
+        -show_entries stream=codec_name  \
+        -of default=noprint_wrappers=1:nokey=1 \
+        "$@"
+}
+
+is_h264 () { test h264 = "$( video_codec_name ${1?path of the media to test} )" }
+
 encoder=( ffmpeg  -v error -nostats )
 available_sizes=(
     320.240
@@ -18,3 +27,5 @@ workdir=.
 
 # TODO: XDG support ?
 for it ( ~/.dtrt-ffpmeg(N) ) . $it
+
+
