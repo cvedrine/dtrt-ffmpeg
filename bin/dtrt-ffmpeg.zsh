ddc07133eb0cd9c644b0ce9984b9fce956b2285c
Marc Chantreux <mc@unistra.fr>
2017-09-04 10:55:00 +0200

    remove some useless fmpeg outputs by default

--- bin/dtrt-ffmpeg.zsh
+++ bin/dtrt-ffmpeg.zsh
@@ -3,13 +3,12 @@ setopt nounset warncreateglobal extendedglob braceccl
 use_slurm=false use_cuvid=false
 output_template='${input:t:r}_${width}x${height}.${input:e}'
 
-encoder=( ffmpeg )
+encoder=( ffmpeg  -v error -nostats )
 available_sizes=(
     320.240
     720.480
     1280.720
-    1920.1080
-)
+    1920.1080 )
 
 workdir=.
 
