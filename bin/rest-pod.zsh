3145cc71b89ee035a5d17ee565490db12cdfea04
Marc Chantreux <mc@unistra.fr>
2017-09-04 10:56:04 +0200

    use  $URL and add height_id()

--- bin/rest-pod.zsh
+++ bin/rest-pod.zsh
@@ -1,7 +1,11 @@
 . rest-base.zsh
 
+URL=https://pod-ws-test.u-strasbg.fr/webservice \
+    TOKEN=739168987f8b65829c0c4eb94620784ac9c2f750 \
+    set_rest_service pod
+
 info () {
-    rest_client+=("$WS/podspod/${1?id of the video}.json")
+    rest_client+=("$URL/podspod/${1?id of the video}.json")
     shift
     "$@"
 }
@@ -11,5 +15,16 @@ encoding_in_progress () {
     shift
     "$@"
 }
+
+pod_encodingtypes        () { route $URL/coreencodingtype "$@" }
+pod_encodings            () { route $URL/podsencodingpods "$@" }
+select_pod_encodingtypes () { jq " .results[] | select( ${1?selector} ) " }
+
+as_height_id () {
+        jq --raw-output ' .[]
+        # | select( .output_height == 1080 )
+        | @text "\(.output_height) \(.id)" '
+}
+
 
 
