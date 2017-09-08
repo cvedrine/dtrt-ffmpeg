5c64ea04d91fc0490de9ffbc0dbe7f6c96853516
Marc Chantreux <mc@unistra.fr>
2017-09-08 08:32:07 +0200

    notes and fixes from the sept 4 meeting

--- bin/dtrt-ffmpeg.zsh
+++ bin/dtrt-ffmpeg.zsh
@@ -1,16 +1,7 @@
 setopt nounset warncreateglobal extendedglob braceccl
 
-use_slurm=false use_cuvid=false
+use_slurm=${use_slurm:-false} use_cuvid=${use_cuvid:-false}
 output_template='${input:t:r}_${width}x${height}.${input:e}'
-
-video_codec_name () {
-    ffprobe -v error -select_streams v:0 \
-        -show_entries stream=codec_name  \
-        -of default=noprint_wrappers=1:nokey=1 \
-        "$@"
-}
-
-is_h264 () { test h264 = "$( video_codec_name ${1?path of the media to test} )" }
 
 encoder=( ffmpeg  -v error -nostats )
 available_sizes=(
