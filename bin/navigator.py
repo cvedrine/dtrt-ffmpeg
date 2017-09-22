ffea269e6bd2b10b014948283e544e296850dbeb
Marc Chantreux <mc@unistra.fr>
2017-09-22 11:14:40 +0200

    nagivator gets set_thumbnails and get_slug methods

--- bin/navigator.py
+++ bin/navigator.py
@@ -6,11 +6,14 @@ from django.core.files import File
 
 class Navigator():
 
-    def relcd(self,name):
+    def cdrel(self,name):
 	self.pwd, _ = Folder.objects.get_or_create\
 	    ( name=name
 	    , owner=self.pwd.owner
 	    , parent=self.pwd )
+        return self.pwd
+
+    def cdpk(self,pk): self.pwd, _ = Folder.objects.get(id=int(pk))
 
     def go_home(self,owner):
 	self.pwd = Folder.objects.get\
@@ -20,7 +23,7 @@ class Navigator():
 
     def go_video_slug(self,video):
 	self.go_home(video.owner)
-	self.relcd(video.slug)
+        return self.cdrel(video.slug)
 
     def store(self,model,source,dest):
 	file, _ = model.objects.get_or_create\
@@ -33,14 +36,24 @@ class Navigator():
 	file.owner = self.pwd.owner
 	return file
 
+def V(v):
+    if isinstance(v,Pod): return v
+    if isinstance(v,str): return Pod.objects.get(id=v)
+    return None
+
 class Command(BaseCommand):
     args = '<video t t ...>'
     help = 'Encodes the specified content.'
 
-    def handle(self, *args, **options):
-	vid = args[0]
-	ts    = args[1:]
-	video = Pod.objects.get(id=vid)
+    def get_slug(self,video):
+        video = V(video)
+        n = Navigator()
+        n.go_video_slug(V(video))
+        p = n.pwd
+        for i in [ p.name, p.pk ]: print(i)
+
+    def set_thumbnails(self,video,*ts):
+	video = V(video)
 	store = Navigator()
 	store.go_video_slug(video)
 	thumbnails =\
@@ -49,4 +62,12 @@ class Command(BaseCommand):
 	for t in thumbnails: t.save()
 	video.thumbnail = thumbnails[1]
 	video.save()
+
+
+
+
+    def handle(self, *args, **options): getattr(self,args[0])(*args[1:])
+
+
+
 
