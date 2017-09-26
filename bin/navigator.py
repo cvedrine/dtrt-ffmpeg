b670e56c4381cb72a40fa56e4ba700a599ad5d18
Marc Chantreux <mc@unistra.fr>
2017-09-26 11:17:08 +0200

    only thumbnails left ?

--- bin/navigator.py
+++ bin/navigator.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
-from pods.models import Pod
+from pods.models import Pod, EncodingPods
 from filer.models.foldermodels import Folder
 from filer.models.imagemodels  import Image
 from django.core.files import File
@@ -10,6 +10,8 @@ import os
 DEVNULL=None
 
 import os
+
+def root_for(base): return os.path.join( settings.MEDIA_ROOT, base )
 
 class Navigator():
 
@@ -37,7 +39,20 @@ class Navigator():
             ( folder = self.wd
             , name   = dest
             , owner  = self.wd.owner )
-        os.rename(source, file.path)
+
+        # sys_dest = os.path.join\
+        #         ( settings.MEDIA_ROOT
+        #         , getattr(settings,'VIDEOS_DIR','videos')
+        #         , self.wd.pretty_logical_path, dest )
+
+        # sys_dest =\
+        #     settings.MEDIA_ROOT\
+        #     + '/' + getattr(settings,'VIDEOS_DIR','videos')\
+        #     + '/' + self.wd.pretty_logical_path\
+        #     + '/' + dest
+
+        # # os.rename( source, sys_dest)
+        # print(" mv from %s -----------------> %s " % ( source, sys_dest))
         return file
 
     def pwd(self):
@@ -48,6 +63,16 @@ def V(v):
     if isinstance(v,Pod): return v
     if isinstance(v,str): return Pod.objects.get(id=v)
     return None
+
+def path_for_items_of_video(video):
+    video = V(video)
+    login = video.owner.username
+    id    = video.id
+    return os.path.join\
+        ( getattr(settings,'VIDEOS_DIR','videos')
+        , login
+        , get_media_guard( login, id )
+        , "%s" % id )
 
 def ressources_root_for(video):
     video = V(video)
@@ -68,7 +93,7 @@ class Command(BaseCommand):
     args = '<video t t ...>'
     help = 'Encodes the specified content.'
 
-    def set_thumbnails(self,video,*ts):
+    def set_thumbnails_for(self,video,*ts):
         video = V(video)
         store = Navigator()
         store.go_video_slug(video)
@@ -79,27 +104,58 @@ class Command(BaseCommand):
         video.thumbnail = thumbnails[1]
         video.save()
 
-    def ressources_root_for(self,video): print(ressources_root_for(video))
+    def ressources_root_for(self,video): print(root_for(path_for_items_of_video(V(video))))
 
     def get_video_scales(self):
         for e in get_video_scales(): print(e)
 
-    def set_video_overview(self,video,filename):
+    def get_video_path(self,video): print(V(video).video.path)
+
+    def add_overview_for(self,video,filename):
+        place = os.path.join\
+            ( path_for_items_of_video(video)
+            , os.path.basename(filename) )
         video = V(video)
-        video.overview = filename
+        video.overview = place
         video.save()
+        print(root_for(place))
 
     def add_encoding_for(self,video,height,filename):
         video = V(video)
-        epod  = EncodingPods.objects.get_or_create\
-            ( video=video
-            , encodingType=
-                EncodingType.objects.filter\
+        place = os.path.join\
+            ( path_for_items_of_video(video)
+            , os.path.basename(filename) )
+
+        etype = EncodingType.objects.filter\
                     ( mediatype='video'
-                    , output_height=height )
+                    , output_height=height )[0]
+
+        epod, DEVNULL  = EncodingPods.objects.get_or_create\
+            ( video=video
+            , encodingType=etype
+            , encodingFile = place
             , encodingFormat="video/mp4")
-        epod.encodingFile = filename
+
         epod.save()
         video.save()
+
+        print(root_for(place))
+
+        # in perl:
+        # say for grep /^[fp]/, keys %$epod
+        #
+        # now ... in python
+
+        # import re
+        # p = re.compile('^[fp]')
+
+        # because the python syntax is weirdly inconsistent:
+
+        # FUCK you python ... round 1
+        # for d in dir(epod) if p.match(d): print(d)
+
+        # FUCK you python ... round 2
+        # for d in dir(epod): if p.match(d): print(d)
+
 
     def handle(self, *args, **options): getattr(self,args[0])(*args[1:])
